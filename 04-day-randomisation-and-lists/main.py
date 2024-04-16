0cdebd48eed90a97e7af64221e622c5075e55cf9
anblackter <angel.henao@outlook.com>
2024-04-16 10:51:12 -0500

    feat(04-day): project solution

--- 04-day-randomisation-and-lists/main.py
+++ 04-day-randomisation-and-lists/main.py
@@ -26,3 +26,21 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
+import random
+input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+options = [rock, paper, scissors]
+computer_choice = random.randint(0, 2)
+print(f"You chose:\n{options[input]}")
+print(f"Computer chose:\n{options[computer_choice]}")
+if input == computer_choice:
+    print("It's a draw.")
+elif input == 0 and computer_choice == 2:
+    print("You win.")
+elif input == 1 and computer_choice == 0:
+    print("You win.")
+elif input == 2 and computer_choice == 1:
+    print("You win.")
+elif input > 2:
+    print("Invalid number. You lose.")
+else:
+    print("You lose.")
