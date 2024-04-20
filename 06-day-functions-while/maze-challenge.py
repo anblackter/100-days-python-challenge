5864a7e245a5e7aa875147886df80f138aa91e25
anblackter <angel.henao@outlook.com>
2024-04-20 18:13:20 -0500

    feat(06-day): maze challenge solution

--- 06-day-functions-while/maze-challenge.py
+++ 06-day-functions-while/maze-challenge.py
@@ -1,1 +1,24 @@
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def face_up():
+    while not is_facing_north():
+        turn_left()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+initial_position = True
+while not at_goal():
+    if initial_position:
+        face_up()
+        initial_position = False
+        
+    if front_is_clear():
+        move()
+    elif right_is_clear():
+        turn_right()
+        move()
+    elif wall_on_right() and wall_in_front():
+        turn_left()
