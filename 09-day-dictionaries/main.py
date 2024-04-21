235231228810c76b55a28d53322a80bf1557f849
anblackter <angel.henao@outlook.com>
2024-04-21 11:17:23 -0500

    feat(09-day): project solution

--- 09-day-dictionaries/main.py
+++ 09-day-dictionaries/main.py
@@ -1,2 +1,22 @@
 from replit import clear
 #HINT: You can call clear() to clear the output in the console.
+
+other_bidders = True
+
+while other_bidders:
+    bidders = {}
+    name = input("What is your name?: ")
+    bid = int(input("What is your bid?: $"))
+    bidders[name] = bid
+    other_bidders = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+    if other_bidders == 'yes':
+        clear()
+    else:
+        other_bidders = False
+        highest_bid = 0
+        winner = ""
+        for bidder in bidders:
+            if bidders[bidder] > highest_bid:
+                highest_bid = bidders[bidder]
+                winner = bidder
+        print(f"The winner is {winner} with a bid of ${highest_bid}")
