fe48e8f0a538aeb2fed2d5bdd1aac38c589ddd24
anblackter <angel.henao@outlook.com>
2024-04-20 19:12:26 -0500

    feat(07-day): challenge 4 solution

--- 07-day-hangman/hangman-04-challenge.py
+++ 07-day-hangman/hangman-04-challenge.py
@@ -66,6 +66,7 @@ word_length = len(chosen_word)
 
 #TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
 #Set 'lives' to equal 6.
+lives = 6
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -86,6 +87,8 @@ while not end_of_game:
             display[position] = letter
 
     #TODO-2: - If guess is not a letter in the chosen_word,
+    if guess not in chosen_word:
+        lives -= 1
     #Then reduce 'lives' by 1. 
     #If lives goes down to 0 then the game should stop and it should print "You lose."
 
@@ -98,3 +101,4 @@ while not end_of_game:
         print("You win.")
 
     #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    print(stages[lives])
