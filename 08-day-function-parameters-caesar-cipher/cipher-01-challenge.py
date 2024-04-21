356505c705a9158a346fc8966330fbc10e75a5ee
anblackter <angel.henao@outlook.com>
2024-04-20 19:52:16 -0500

    feat(08-day): ceaser cipher challenge 1 solution

--- 08-day-function-parameters-caesar-cipher/cipher-01-challenge.py
+++ 08-day-function-parameters-caesar-cipher/cipher-01-challenge.py
@@ -4,9 +4,20 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+#Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+def encrypt(text, shift):
+    shift_word = ''
+    for letter in text:
+        index = alphabet.index(letter)
+        if (index + shift) < len(alphabet):
+            shift_letter = alphabet[index + shift]
+        else:
+            shift_letter = alphabet[index + shift - len(alphabet)]
+        shift_word += shift_letter
+    print(f"The encoded text is {shift_word}")
+
+    #Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
     #e.g. 
     #plain_text = "hello"
     #shift = 5
@@ -18,4 +29,5 @@ shift = int(input("Type the shift number:\n"))
 
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+#Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+encrypt(text, shift)
