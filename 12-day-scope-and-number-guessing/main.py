b56c7cf33ec5d1468d1da75c6e8bae5929ebdecc
anblackter <angel.henao@outlook.com>
2024-04-24 20:33:30 -0500

    feat(12-day): project solution

--- 12-day-scope-and-number-guessing/main.py
+++ 12-day-scope-and-number-guessing/main.py
@@ -8,3 +8,37 @@
 # If they run out of turns, provide feedback to the player. 
 # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 
+import random
+from art import logo
+
+number = random.randint(1,100)
+attemps = 0
+guessed = False
+
+print(logo)
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+difficulty = input('Choose a difficulty. Type "easy" or "hard": ')
+if difficulty == 'easy':
+    attemps = 10
+else:
+    attemps = 5
+
+while attemps > 0 and not guessed:
+    print(f"You have {attemps} remaining to guess the number.")
+    guess = int(input("Make a guess: "))
+
+    if guess > number:
+        print("Too high.")
+        attemps -= 1
+        print("Guess Again.")
+    elif guess < number:
+        print("Too low.")
+        attemps -= 1
+        print("Guess Again.")
+    elif guess == number:
+        print(f"You got it! The answer was {number}.")
+        guessed = True
+
+    if attemps == 0:
+        print("You've run out of guesses, you lose.")
