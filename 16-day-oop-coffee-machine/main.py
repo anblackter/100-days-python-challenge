3b748d9546017eb56ed2fb109b98500bd23e2444
anblackter <angel.henao@outlook.com>
2024-04-27 22:20:51 -0500

    feat(16-day): project solution

--- 16-day-oop-coffee-machine/main.py
+++ 16-day-oop-coffee-machine/main.py
@@ -1,3 +1,22 @@
 from menu import Menu, MenuItem
-from coffee_maker import CoffeeMaker
+from coffe_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+is_on = True
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+while is_on:
+    choice = input(f"What would you like? ({menu.get_items()}): ")
+
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(order_name=choice)
+        if coffee_maker.is_resource_sufficient(drink=drink):
+            if money_machine.make_payment(cost=drink.cost):
+                coffee_maker.make_coffee(order=drink)
